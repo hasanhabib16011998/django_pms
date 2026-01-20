9a056d61f9e78f61238a90c4a177d80140d69638
hasanhabib16011998 <hasan16habib@gmail.com>
2026-01-20 18:39:40 +0600

    projects module lastest projects

---
+++ Readme.md
@@ -0,0 +1,3 @@
+username: admin
+password: admin2026
+email: admin@vertical-innovations.com
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import View
+from projects.models import *
 
 # Create your views here.
 class DashBoardView(View):
     def get(self, request, *args, **kwargs):
         context={}
-        context["name"] = "Hasan"
-        context["email"] = "hasan@vertical-innovations.com"
+        latest_projects = Project.objects.all()[:5]
+        context['latest_projects'] = latest_projects
         return render(request,"accounts/dashboard.html", context)
